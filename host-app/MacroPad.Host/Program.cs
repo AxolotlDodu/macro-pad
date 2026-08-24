3cb45d7809094a5f00c2a5aa893f9db219252dc2
ponsy <yohanpons07@gmail.com>
2026-08-24 21:00:27 +0200

    Add host-app skeleton: HID device listing with HidSharp

---
+++ host-app/MacroPad.Host/Program.cs
@@ -0,0 +1,16 @@
+﻿using HidSharp;
+
+var devices = DeviceList.Local.GetHidDevices();
+
+Console.WriteLine("Périphériques HID détectés :\n");
+
+foreach (var device in devices)
+{
+    Console.WriteLine($"{device.GetFriendlyName()}");
+    Console.WriteLine($"  VID: 0x{device.VendorID:X4}  PID: 0x{device.ProductID:X4}");
+    Console.WriteLine($"  Path: {device.DevicePath}");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Appuie sur une touche pour quitter...");
+Console.ReadKey();
