fb0be93d3562e31282f578bf039116db3eab424e
ponsy <yohanpons07@gmail.com>
2026-08-25 13:42:10 +0200

    feat: Add Avalonia tray icon (background app, left-click to quit)

--- host-app/MacroPad.Host/App.axaml.cs
+++ host-app/MacroPad.Host/App.axaml.cs
@@ -13,42 +13,21 @@ public partial class App : Application
     public override void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-        {
             desktop.ShutdownMode = ShutdownMode.OnExplicitShutdown;
 
-            // Fenêtre invisible requise sous Windows pour que le menu du tray icon
-            // ait un handle valide auquel s'accrocher.
-            desktop.MainWindow = new Window
-            {
-                Width = 0,
-                Height = 0,
-                ShowInTaskbar = false,
-                WindowDecorations = Avalonia.Controls.WindowDecorations.None,
-                WindowStartupLocation = WindowStartupLocation.Manual,
-                Position = new PixelPoint(-10000, -10000)
-            };
-        }
+        var trayIcons = TrayIcon.GetIcons(this);
+        var trayIcon = trayIcons![0];
+        trayIcon.Icon = new WindowIcon(AssetLoader.Open(new Uri("avares://MacroPad.Host/Assets/tray.ico")));
 
-        _cts = new CancellationTokenSource();
-        _ = Task.Run(() => new PadService(_cts.Token).RunAsync(), _cts.Token);
-
-        var quitItem = new NativeMenuItem("Quitter");
-        quitItem.Click += (_, _) =>
+        trayIcon.Clicked += (_, _) =>
         {
             _cts?.Cancel();
             if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime lifetime)
                 lifetime.Shutdown();
         };
 
-        var trayIcon = new TrayIcon
-        {
-            Icon = new WindowIcon(AssetLoader.Open(new Uri("avares://MacroPad.Host/Assets/tray.ico"))),
-            ToolTipText = "MacroPad",
-            Menu = new NativeMenu { quitItem }
-        };
-
-        var trayIcons = new TrayIcons { trayIcon };
-        TrayIcon.SetIcons(this, trayIcons);
+        _cts = new CancellationTokenSource();
+        _ = Task.Run(() => new PadService(_cts.Token).RunAsync(), _cts.Token);
 
         base.OnFrameworkInitializationCompleted();
     }
